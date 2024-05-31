f21359b5e8891280c6f26324d9dae6a8fe9bc281
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-05-31 18:34:30 +0530

    ADD

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+let str = prompt("Enter a String");
+
+let i =0;
+let j =0;
+
+while(i < str.length){
+  if(str[i] === str[j]){
+    i++;
+  }
+  else{
+    if(i-j <= 1){
+      console.log(str[j]);
+      break;
+    }
+    j=i;
+    i++;
+  }
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
